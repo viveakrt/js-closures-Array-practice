c09a8a209799a6a5e3ac28bc7c4b035181080903
Vivek <vivek.15@mountblue.tech>
2021-02-11 18:27:13 +0530

    Added map fuction which creates a new array populated with the results of calling a provided function on every element in the calling array

--- arrays.js
+++ arrays.js
@@ -9,5 +9,18 @@ function each(elements, cb){
     }
     return undefined
 }
-
+/**
+ * This method creates a new array populated with the results of calling a provided function on every element in the calling array.
+ * @param {array} elements 
+ * @param {function} cb 
+ */
+function map(elements, cb) {
+    let array = [];
+    for(let i=0;i < elements.length ; i++){
+        let value = cb(elements[i], i, elements);
+        array.push(value);
+    }
+    return array;
+}
+module.exports = map
 module.exports = each
