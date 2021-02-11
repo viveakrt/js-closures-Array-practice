f82a34cf4404d07c8836f4fd5ffb968f40b21167
Vivek <vivek.15@mountblue.tech>
2021-02-11 20:07:22 +0530

    Added limitFuctionCallCount invokes only limited number of times

--- closures.js
+++ closures.js
@@ -1,3 +1,6 @@
+/**
+ * Return an object that has two methods called `increment` and `decrement`.
+ */
 function counterFactory() {
     return {
         count: 0,
@@ -9,3 +12,22 @@ function counterFactory() {
         }
     }
 }
+/**
+ * Return a function that invokes callback
+ * @param {function} cb 
+ * @param {number} n 
+ */
+function limitFunctionCallCount(cb, n) {
+    let count = counterFactory();
+    function invoke() {
+        if (count.count < n) {
+            count.increment();
+            return cb();
+        }else {
+            return null;
+        }
+    }
+    return invoke;
+}
+
+module.exports = {counterFactory,limitFunctionCallCount}
